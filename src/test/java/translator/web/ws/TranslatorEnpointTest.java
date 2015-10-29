947c4c692baa8f99cce41cc769d1612cccf0bcf4
Adrian <650220@unizar.es>
2015-10-29 13:29:19 +0100

    Changes in the text to translate

--- src/test/java/translator/web/ws/TranslatorEnpointTest.java
+++ src/test/java/translator/web/ws/TranslatorEnpointTest.java
@@ -42,11 +42,11 @@ public class TranslatorEnpointTest {
 		GetTranslationRequest request = new GetTranslationRequest();
 		request.setLangFrom("en");
 		request.setLangTo("es");
-		request.setText("This is a test of translation service");
+		request.setText("Tomorrow I'll travel to St. Petersburg");
 		Object response = new WebServiceTemplate(marshaller).marshalSendAndReceive("http://localhost:"
 				+ port + "/ws", request);
 		assertNotNull(response);
 		assertThat(response, instanceOf(GetTranslationResponse.class));
-		assertThat(((GetTranslationResponse) response).getTranslation(), is("Esto es una prueba de servicio de traducción"));
+		assertThat(((GetTranslationResponse) response).getTranslation(), is("Mañana me voy de viaje a San Petersburgo"));
 	}	
 }
